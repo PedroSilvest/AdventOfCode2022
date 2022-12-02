f13462455708516d640e6eef4c712d18c717c685
Silvestre <80043583+PedroSilvest@users.noreply.github.com>
2022-12-02 22:36:46 +0000

    Create day1.py

---
+++ day1/day1.py
@@ -0,0 +1,24 @@
+def task_1():
+    """find elf with more item calories"""
+    file = open("day1/input.txt", "r")
+    count = 0
+    save = []
+    for line in file.readlines():
+        if line.replace("\n","").isnumeric():
+            count += int(line.replace("\n",""))
+        else:
+            save.append(count)
+            count = 0
+    save.append(count)
+    return sorted(save, reverse=True)
+
+
+
+def task_2(cal):
+    """sum of the item calories of the 3 elfs with most calories"""
+    return sum(calories[:3])
+
+
+calories = task_1()
+print(calories[0])
+print(task_2(calories))
